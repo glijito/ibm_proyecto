99e92cc41fe632280074d6675975106461391b73
glijito <meroser.gs@gmail.com>
2023-03-04 13:14:13 -0600

    Cuarto commit - Generacion de cards y listview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ibm_proyecto/screens/listview2_screen.dart';
+import 'screens/screens.dart';
 
 void main() {
     runApp(const MyApp());
@@ -10,9 +10,15 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-      return const MaterialApp(
+      return  MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Listview2Screen()
+        home: Listview2Screen(),
+        routes:{
+          'listview1': (BuildContext context ) => const Listview1Screen(),
+          'listview2': (BuildContext context ) => const Listview2Screen(),
+          'alert_screen': (BuildContext context ) => const AlertScreen(),
+          'card_screen': (BuildContext context ) => const CardScreen(),
+        }
       );
     }
 }
---
+++ lib/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:ibm_proyecto/screens/listview1_screen.dart';
+export 'package:ibm_proyecto/screens/listview2_screen.dart';
+export 'package:ibm_proyecto/screens/alert_screen.dart';
+export 'package:ibm_proyecto/screens/card_screen.dart';
